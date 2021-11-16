e2815f678ea538c9b5876c5693132340a59179c9
柊斗 金賀 <190666@jc-21.jp>
2021-11-16 12:41:56 +0900

    中身変更

--- R03BMI/MainPage.xaml.cs
+++ R03BMI/MainPage.xaml.cs
@@ -18,7 +18,26 @@ namespace R03BMI
 
         private void Button_Clicked(object sender, EventArgs e)
         {
-
+            var height = heighit.Text;
+            var weight = weighit.Text;
+            try
+            {
+                double h = double.Parse(height);
+                if (h >= 100)
+                {
+                    h = h / 100;
+                }
+                double w = double.Parse(weight);
+                double bmi = w / h / h;
+                int bmi2 = (int)(bmi * 10);
+                double bmi3 = (double)(bmi2 / 10.0);
+                result.Text = "身長" + h + "、体重" + w + "の人のBMIは" + bmi3 + "です。";
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex);
+                result.Text = "！！！";
+            }
         }
     }
 }
